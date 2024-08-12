733c87256713ebc0efa8579acb14be795b2f588a
hungvuong11 <hungvuong@technixo.com>
2024-08-12 23:44:19 +0700

    feat: update params withdraw

--- src/features/tap-game/hooks/useWithdraw.ts
+++ src/features/tap-game/hooks/useWithdraw.ts
@@ -1,7 +1,9 @@
 import {useSendSocketRequest} from "@/libs/hooks/useSendSocketRequest";
 import {SocketRoutes} from "@/libs/redux/features/socketSlice";
+import {useGetCurrentUser} from "@/libs/hooks/useGetCurrentUser";
 
 export const useWithdraw = () => {
+  const {currentUser} = useGetCurrentUser()
   const { trigger, loading } = useSendSocketRequest({
     route: SocketRoutes.RequestWithdraw,
     enable: false,
@@ -9,7 +11,7 @@ export const useWithdraw = () => {
   });
 
   const handleWithdraw = ({amount, receiver, chain_id}: {amount: number, receiver: string, chain_id: number}) => {
-    trigger({amount, receiver, chain_id, token_id: 1});
+    trigger({amount, receiver, chain_id, token_id: 1, user_id: currentUser?.id});
   }
 
   return { handleWithdraw, loading }
