a961a377c0660c262dbfa36a16754b2a24ee121d
peih <hiepdh@technixo.com>
2024-08-15 09:43:18 +0700

    fix: fix minor ui bugs

--- src/features/tap-game/layout/index.tsx
+++ src/features/tap-game/layout/index.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { cn } from "@/utils/cn";
+import {useGetCurrentUser} from "@/libs/hooks/useGetCurrentUser";
 
 const LayoutTapGame = ({ children }: { children: React.ReactNode }) => {
+  const { currentUser } = useGetCurrentUser();
   return (
     <div className={cn("mx-auto	 h-[100vh] w-full max-w-[500px] bg-black")}>
       <div
@@ -9,7 +11,7 @@ const LayoutTapGame = ({ children }: { children: React.ReactNode }) => {
           "hide-scrollbar relative h-full overflow-auto",
         )}
         style={{
-          maxHeight: "calc(100vh - 90px)"
+          maxHeight: currentUser ? "calc(100vh - 82px)" : "100vh",
         }}
       >
         {children}
