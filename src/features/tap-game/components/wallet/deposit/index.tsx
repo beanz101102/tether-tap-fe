9f4853581f9a367cc8f92bb414ea5f1138eab920
hungvuong11 <hungvuong@technixo.com>
2024-08-15 17:02:31 +0700

    feat: update ui withdraw

--- src/features/tap-game/components/wallet/deposit/index.tsx
+++ src/features/tap-game/components/wallet/deposit/index.tsx
@@ -1,21 +1,22 @@
 "use client";
-import {useGetCurrentUser} from "@/libs/hooks/useGetCurrentUser";
-import {useAtom} from "jotai/index";
-import {Button} from "@/components/ui/button";
+import { useGetCurrentUser } from "@/libs/hooks/useGetCurrentUser";
+import { useAtom } from "jotai/index";
+import { Button } from "@/components/ui/button";
 import SelectChain from "../SelectChain";
 import ListHistory from "../ListHistory";
-import {useTranslation} from "@/app/[lng]/i18n/client";
-import {truncateAddress} from "@/utils/truncateAddress";
-import {useWindowSize} from "@/features/tap-game/hooks/useWindowSize";
-import {copyToClipboardWithCommand} from "@/utils/copyToClipboardWithCommand";
-import {useEffect, useState} from "react";
-import {api} from "@/trpc/react";
-import {uniqBy} from "lodash";
-import {atom} from "jotai";
-import {ITransferTransactionHistory} from "@/features/tap-game/interfaces/transaction-history";
+import { useTranslation } from "@/app/[lng]/i18n/client";
+import { truncateAddress } from "@/utils/truncateAddress";
+import { useWindowSize } from "@/features/tap-game/hooks/useWindowSize";
+import { copyToClipboardWithCommand } from "@/utils/copyToClipboardWithCommand";
+import { useEffect, useState } from "react";
+import { api } from "@/trpc/react";
+import { uniqBy } from "lodash";
+import { atom } from "jotai";
+import { ITransferTransactionHistory } from "@/features/tap-game/interfaces/transaction-history";
 
-const listDepositTransactionHistoryAtom = atom<ITransferTransactionHistory[]>([]);
-
+const listDepositTransactionHistoryAtom = atom<ITransferTransactionHistory[]>(
+  [],
+);
 
 const Deposit = () => {
   const { t } = useTranslation("tap-game", {
@@ -24,7 +25,6 @@ const Deposit = () => {
   const { currentUser } = useGetCurrentUser();
   const { width } = useWindowSize();
   const address = currentUser?.address;
-
 
   return (
     <div className={"flex w-full flex-col items-center justify-center px-4"}>
@@ -47,7 +47,7 @@ const Deposit = () => {
         </p>
         <div className="flex items-center justify-between">
           <div className="main-bg-default main-text-primary main-border-color w-[65%] rounded-md border px-3 py-2 text-base font-normal">
-            {truncateAddress(address, width < 780 ? 6 : 10)}
+            {truncateAddress(address, width < 390 ? 7 : 10)}
           </div>
           <Button
             onClick={() => copyToClipboardWithCommand(address ?? "")}
@@ -58,7 +58,7 @@ const Deposit = () => {
           </Button>
         </div>
       </div>
-      <ListTransactionHistory/>
+      <ListTransactionHistory />
     </div>
   );
 };
@@ -67,24 +67,29 @@ const ListTransactionHistory = () => {
   const { currentUser } = useGetCurrentUser();
 
   const [page, setPage] = useState(1);
-  const [listDepositTransactionHistory, setListDepositTransactionHistory] = useAtom(listDepositTransactionHistoryAtom);
-  const { data, isLoading } = api.tapGame.getListTransactionHistory.useQuery({
-    address: currentUser?.address as string,
-    pageSize: 20,
-    page: page,
-    status : 'deposit'
-  }, {
-    refetchInterval: 10000
-  });
+  const [listDepositTransactionHistory, setListDepositTransactionHistory] =
+    useAtom(listDepositTransactionHistoryAtom);
+  const { data, isLoading } = api.tapGame.getListTransactionHistory.useQuery(
+    {
+      address: currentUser?.address as string,
+      pageSize: 20,
+      page: page,
+      status: "deposit",
+    },
+    {
+      refetchInterval: 10000,
+    },
+  );
 
   useEffect(() => {
     if (!data || isLoading) return;
     if (page === 1) {
       setListDepositTransactionHistory(data?.listTransactionHistory as any[]);
     } else {
-      setListDepositTransactionHistory(
-        [...listDepositTransactionHistory, ...(data?.listTransactionHistory as any)]
-      );
+      setListDepositTransactionHistory([
+        ...listDepositTransactionHistory,
+        ...(data?.listTransactionHistory as any),
+      ]);
     }
   }, [data, page, isLoading]);
 
@@ -94,8 +99,9 @@ const ListTransactionHistory = () => {
       hasMore={data?.listTransactionHistory?.length === 20}
       isLoading={isLoading}
       nextPage={() => setPage(page + 1)}
-      title="deposit_history" />
-  )
-}
+      title="deposit_history"
+    />
+  );
+};
 
 export default Deposit;
