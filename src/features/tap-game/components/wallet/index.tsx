9d5078a36d5833b62343df09810b59132b0bcff5
hungvuong11 <hungvuong@technixo.com>
2024-08-16 15:48:46 +0700

    fix build app

--- src/features/tap-game/components/wallet/index.tsx
+++ src/features/tap-game/components/wallet/index.tsx
@@ -14,6 +14,7 @@ import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import ListHistory from "./ListHistory";
 import SelectChain, { ChainIdAtom } from "./SelectChain";
+import AnimatedNumber from "@/components/ui/animated-number";
 
 const listTransactionHistoryAtom = atom<ITransferTransactionHistory[]>([]);
 
